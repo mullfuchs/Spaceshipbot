a6846cfc638f098f39396324ddd4caded04459af
mullfuchs <burke.leo@gmail.com>
2016-11-15 15:49:02 -0800

    breaking things

--- bot.js
+++ bot.js
@@ -22,39 +22,39 @@ function sendTweet() {
   // var command = 'processing-java --sketch=`pwd`/imageCreator --run';
 
   // exec(command, processing);
-  fs.readFile('/path/to/sketch.pde', function(err, data) {
+  fs.readFile('../imageCreator/imageCreator.pde', function(err, data) {
     var compiled = Processing.compile(data.toString('utf-8'))
       , p5 = new Processing(canvas, compiled)
-      , out = fs.createWriteStream('/path/to/output.png')
+      , out = fs.createWriteStream('../path/to/output.png')
       , stream = canvas.createPNGStream();
  
     stream.pipe(out);
-  }
+  });
 
-  function processing() {
+  // function processing() {
 
-      var filename = 'imageCreator/output.png';
-      var params = {
-        encoding: 'base64'
-      }
-      var b64Content = fs.readFileSync(filename, params);
-      T.post('media/upload', {media_data: b64Content}, uploaded); 
+  //     var filename = 'imageCreator/output.png';
+  //     var params = {
+  //       encoding: 'base64'
+  //     }
+  //     var b64Content = fs.readFileSync(filename, params);
+  //     T.post('media/upload', {media_data: b64Content}, uploaded); 
 
-      function uploaded(err, data, response) {
-        // body...
-        var id = data.media_id_string;
+  //     function uploaded(err, data, response) {
+  //       // body...
+  //       var id = data.media_id_string;
 
-        var rando = Math.floor(Math.random()*100);
+  //       var rando = Math.floor(Math.random()*100);
 
-        var message = { 
-          status: 'Testing run! random image ' + rando,
-          media_ids: [id]
-        }
+  //       var message = { 
+  //         status: 'Testing run! random image ' + rando,
+  //         media_ids: [id]
+  //       }
 
-        T.post('statuses/update', message, tweeted);
-      }
-      console.log('created image');
-    }
+  //       T.post('statuses/update', message, tweeted);
+  //     }
+  //     console.log('created image');
+  //   }
 
 
   function tweeted(err, data, response) {
